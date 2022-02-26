7392f0a351ee9619ea1306bd4417e2f7f78358ca
Tobi <47112773+Tobylarry@users.noreply.github.com>
2022-02-25 21:18:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-
+let player1 = 0;
+let player2 = 0;
+let draw = 0;
 
 function check(){
     let a = Math.round(Math.random() * 5 +1);
 let b = Math.round(Math.random() * 5 +1);
 let newImage = './images/dice' + a + '.png';
 let newImage2 = './images/dice'+ b + '.png';
-console.log(newImage);
+
 
 document.getElementById('image').src = newImage;
 document.getElementById('image2').src = newImage2;
 
     if(a === b){
        document.getElementById('header').innerHTML = "Draw!"
+      
     }else if(a > b){
         document.getElementById('header').innerHTML = "🚩 Player 1 wins!"
+        player1++;
+        document.getElementById('p1').innerHTML = "Score: " + player1;
     }
     else if( a < b){
         document.getElementById('header').innerHTML = "Player 2 wins! 🚩"
+        player2++;
+        document.getElementById('p2').innerHTML = "Score: " + player2;
     }
     }
-
